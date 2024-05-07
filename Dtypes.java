59efa1793fa74e8230780d1217069b3d97653a83
Anuhya12338 <128910993+Anuhya12338@users.noreply.github.com>
2024-05-07 13:13:08 +0530

    Create Dtypes.java

---
+++ Dtypes.java
@@ -0,0 +1,13 @@
+package pack1;
+
+public class Dtypes {
+	public static void main(String args[]) {
+		int i=-22;
+		char ch='a';
+		double d = 2223345.6342436;
+		System.out.println("integer value:" + i);
+		System.out.println("charecter value:" + ch);
+		System.out.println("double value:" + d);
+	}
+
+}
