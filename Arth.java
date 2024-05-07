619f9fb944a86f31e7cdf7dc51ba50e9260261f5
Anuhya12338 <128910993+Anuhya12338@users.noreply.github.com>
2024-05-07 13:15:50 +0530

    Create Arth.java

---
+++ Arth.java
@@ -0,0 +1,11 @@
+package pack1;
+
+public class Arth {
+	public static void main(String args[]) {
+		byte a=45;
+		byte b=32;
+		System.out.println("difference is " + (a-b));
+		System.out.println("product is " + (a*b));
+	}
+
+}
