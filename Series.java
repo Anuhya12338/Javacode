6de32fa1076e76dfdf1801eee9115a033f2883b2
Anuhya12338 <128910993+Anuhya12338@users.noreply.github.com>
2024-05-07 13:10:29 +0530

    Create Series.java

---
+++ Series.java
@@ -0,0 +1,16 @@
+package pack1;
+
+public class Series {
+	public static void main (String arge[]) {
+		int rows = 4;
+		for(int i=0;i<=rows;i++)
+		{
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
