6943494b61416dcbc88b24281b85fcacfa9feadc
Anuhya12338 <128910993+Anuhya12338@users.noreply.github.com>
2024-05-07 13:13:48 +0530

    Create Doubleintmul.java

---
+++ Doubleintmul.java
@@ -0,0 +1,11 @@
+package pack1;
+
+public class Doubleintmul {
+	public static void main(String args[]) {
+		double b=8.2;
+		int i =6;
+		double product = (b*i);
+		System.out.println(product);
+	}
+
+}
