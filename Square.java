acfcd8615b747afbe8c7a5a72eb5640914bd1b4d
Anuhya12338 <128910993+Anuhya12338@users.noreply.github.com>
2024-05-07 13:11:18 +0530

    Create Square.java

---
+++ Square.java
@@ -0,0 +1,11 @@
+package pack1;
+
+public class Square {
+	public static void main (String args[])
+	{
+		double d =3.9;
+		double s = d * d;
+		System.out.println("square od 3.9 is " + s);
+	}
+
+}
