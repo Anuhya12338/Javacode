efed4388cd60bdf00dcd361351783c21b75e0004
Anuhya12338 <128910993+Anuhya12338@users.noreply.github.com>
2024-05-09 15:54:35 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,19 @@
+package Training;
+
+public class Main {
+	public static void main(String[] args) {
+     
+        Parent parentObj = new Parent();
+       
+        parentObj.method1();
+        
+        
+        Child childObj = new Child();
+        
+        childObj.Method2();
+        
+        
+        childObj.method1();
+
+}
+}
