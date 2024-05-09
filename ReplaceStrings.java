86fa362f23e10880ee56dbde5809a55a3eacacfe
Anuhya12338 <128910993+Anuhya12338@users.noreply.github.com>
2024-05-09 13:35:00 +0530

    Create ReplaceStrings.java

---
+++ ReplaceStrings.java
@@ -0,0 +1,10 @@
+package Training;
+
+public class ReplaceStrings {
+	public static void main(String[] args){
+
+	    String message = "That was great - lol.";
+String replace=message.replaceAll("lol","laugh out loud");
+System.out.println(replace);
+	}
+	}
