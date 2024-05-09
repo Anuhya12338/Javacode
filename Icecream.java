9037819e4c8397b5cf6d7ff26db03684ed931962
Anuhya12338 <128910993+Anuhya12338@users.noreply.github.com>
2024-05-09 13:30:29 +0530

    Create Icecream.java

---
+++ Icecream.java
@@ -0,0 +1,16 @@
+package Training;
+
+public class Icecream {
+	public static void main(String args[]) {
+		
+	int money = 100;
+	int favicecost = 50;
+	if(money > favicecost) {
+		System.out.println( "yummy");
+	}
+		else {
+			System.out.println("let me buy next time");
+		}
+	}
+
+}
