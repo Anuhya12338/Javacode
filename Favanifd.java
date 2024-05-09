28e98be45325691236b0d7b4ae57a7eade1fb799
Anuhya12338 <128910993+Anuhya12338@users.noreply.github.com>
2024-05-09 13:28:24 +0530

    Create Favanifd.java

---
+++ Favanifd.java
@@ -0,0 +1,15 @@
+package Training;
+
+public class Favanifd {
+	public static void main(String[] args) {
+
+	    String animal ="tiger";
+	    
+	    String food ="penne red sause pasta";
+	    
+	    System.out.println("my fav animal is " + animal + " and food is " + food);
+	    //another way to concat
+        System.out.println(animal.concat(" ").concat("and ").concat( food));
+	}
+
+}
