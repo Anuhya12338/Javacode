4bbe4600540f96ecd937a8dbf4afa0a0051b9043
Anuhya12338 <128910993+Anuhya12338@users.noreply.github.com>
2024-05-09 13:36:31 +0530

    Create Task.java

---
+++ Task.java
@@ -0,0 +1,63 @@
+package Training;
+
+public class Task {
+	public static void main(String[] args) { 
+		String name = "Julian";
+		 String color = "green";
+
+		 String food = "pizza";
+
+		 	System.out.println(name + "'s favorite color is " + color + "." + "His favorite food is " + food );
+		 	
+
+	}
+}
+
+	/*
+	 1.String word ="java";//crt
+
+String Word = " comp";//variable name shld nt start with capital
+
+String word@ = “java string”;//should nt include spl symbols
+3.String word12 = “java string”;//crt
+
+4.String 12word = “java string”;should nt start with num
+
+String int = “java”;//crt
+
+String *java = “java”;//specialsymbol
+
+String symbol$ = “100 dollars”;//crt
+
+8.String word ="java";//crt
+
+String word = " comp";//crt
+
+String word_ =”java”;//crt
+10.String word- =”java is here”;//special symbol
+
+String capital; = “delhi”;//special symbol
+OPERATOR TASKS: PREDICT THE OUTPUTS OF EACH PRINT STATEMENT
+
+int a = 9;
+
+a++;
+
+System.out.println (a);//10
+
+a–-;
+
+System.out.println (a);//9
+
+++a;
+
+System.out.println (a);//10
+
+System.out.println (a++);//10 a=11
+
+System.out.println (a--);//11 a=10
+
+System.out.println (--a);//9
+
+System.out.println (++a); //10
+	 */
