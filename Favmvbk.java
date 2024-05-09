2b408da03e574c53345c3ad95b4a0029744920d9
Anuhya12338 <128910993+Anuhya12338@users.noreply.github.com>
2024-05-09 13:29:02 +0530

    Create Favmvbk.java

---
+++ Favmvbk.java
@@ -0,0 +1,13 @@
+package Training;
+
+public class Favmvbk {
+	public static void main(String[] args) {
+
+	    String movie = "Dear Comrade";
+	    
+	    String book = "Half Girl Friend ";
+	    
+	    System.out.println("my fav movie is " + movie + " and book is " + book);
+	    
+	}
+	}
